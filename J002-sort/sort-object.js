8ea19e1af478804e3b167191cb7b4f9980785f12
TheVirtuoid <thevirtuoid@gmail.com>
2020-05-17 15:37:56 -0400

    Added J002

---
+++ J002-sort/sort-object.js
@@ -0,0 +1,32 @@
+const animals = [
+	{ type: "Cat", name: "Fluffy" },
+	{ type: "Dog", name: "Fido" },
+	{ type: "Horse", name: "Mr. Ed" },
+	{ type: "Cow", name: "Betsy" },
+	{ type: "Coyote", name: "Wile E." },
+	{ type: "Road Runner", name: "Beep Beep" },
+	{ type: "Dolphin", name: "Flipper" },
+	{ type: "Whale", name: "Moby Dick" },
+	{ type: "Lizard", name: "Larry" }
+];
+
+
+animals.sort( function (firstValue, secondValue) {
+	return firstValue.name < secondValue.name ? -1 : firstValue.name > secondValue.name ? 1 : 0;
+});
+animals.forEach( animal => console.log(animal.name));
+
+
+/*
+function sortBy (field) {
+	animals.sort( function (firstValue, secondValue) {
+		return firstValue[field] < secondValue[field] ? -1 : firstValue[field] > secondValue[field] ? 1 : 0;
+	});
+	animals.forEach( animal => console.log(animal[field]));
+}
+
+console.log('\n\n----------BY TYPE-----------');
+sortBy('type');
+console.log('\n\n----------BY NAME-----------');
+sortBy('name');
+*/
---
+++ J002-sort/sort.js
@@ -0,0 +1,25 @@
+const animals = [
+	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
+];
+
+animals.sort( function (firstValue, secondValue) {
+	if (firstValue < secondValue) {
+		return -1;
+	} else if (firstValue > secondValue) {
+		return 1;
+	} else {
+		return 0;
+	}
+});
+animals.forEach( animal => console.log(animal));
+
+
+
+
+
+/*
+animals.sort( function (firstValue, secondValue) {
+	return firstValue < secondValue ? -1 : firstValue > secondValue ? 1 : 0;
+});
+animals.forEach( animal => console.log(animal));
+*/
