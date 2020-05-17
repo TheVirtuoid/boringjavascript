f02b16fe49d0af582a6491b2f5e040af7900428e
TheVirtuoid <thevirtuoid@gmail.com>
2020-05-17 11:01:08 -0400

    Episode J001

---
+++ episode-J001/for.js
@@ -0,0 +1,8 @@
+const animals = [
+	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
+];
+
+let numAnimals = animals.length;
+for (let animalIndex = 0; animalIndex < numAnimals; animalIndex++ ) {
+	console.log(`Animal Number ${animalIndex} is ${animals[animalIndex]}`);
+}
---
+++ episode-J001/forEach.js
@@ -0,0 +1,7 @@
+const animals = [
+	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
+];
+
+animals.forEach( function (animal, animalIndex) {
+	console.log(`Animal Number ${animalIndex} is ${animal}`);
+});
