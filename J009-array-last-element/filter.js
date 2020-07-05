4c634ea79f1a1e037a62a7b3d5ac914197e5a26a
TheVirtuoid <thevirtuoid@gmail.com>
2020-07-05 14:29:45 -0400

    Fixes to J009

--- J009-array-last-element/filter.js
+++ J009-array-last-element/filter.js
@@ -2,22 +2,14 @@ const animals = [
 	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
 ];
 
-if (!Array.isArray(animals)) {
-	console.error('ERROR: Your variable is not an Array!');
-} else {
-	const result = animals.filter( (animal, animalIndex, animalArray) => {
-		let animalsLength = animalArray.length;
-		if (animalIndex === animalsLength - 1) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-	if (result.length !== 1) {
-		console.error(`ERROR: Your variable has two ending elements or was empty! If there were two ending elements ... I'm impressed!`);
+const result = animals.filter( (animal, animalIndex, animalArray) => {
+	let animalsLength = animalArray.length;
+	if (animalIndex === animalsLength - 1) {
+		return true;
 	} else {
-		const myAnimal = result[result.length - 1];
-		console.log(`The last animal in my list is ${myAnimal}`);
+		return false;
 	}
-}
+});
+const myAnimal = result[result.length - 1];
+console.log(`The last animal in my list is ${myAnimal}`);
 
--- J009-array-last-element/pop.js
+++ J009-array-last-element/pop.js
@@ -6,4 +6,3 @@ let myAnimal = animals.pop();
 animals.push(myAnimal);
 
 console.log(`The last animal in my list is ${myAnimal}`);
-console.log(animals);
