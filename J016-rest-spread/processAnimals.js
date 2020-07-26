7b437f56aa72f3f33ab4937b507b9b6ec483ec18
TheVirtuoid <thevirtuoid@gmail.com>
2020-07-26 10:40:11 -0400

    J016 change to processAnimals

--- J016-rest-spread/processAnimals.js
+++ J016-rest-spread/processAnimals.js
@@ -1,4 +1,4 @@
-module.exports = function processAnimals() {
+module.exports = function processAnimals(animal1, animal2, animal3, animal4, animal, animal6, animal7, animal, animal9) {
 	const animals = [].slice.call(arguments);
 	let selectedAnimal = "";
 	animals.forEach( (animal) => {
@@ -8,3 +8,33 @@ module.exports = function processAnimals() {
 	});
 	return selectedAnimal;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+module.exports = function processAnimals(...animals) {
+	let selectedAnimal = "";
+	animals.forEach( (animal) => {
+		if (animal.length > selectedAnimal.length) {
+			selectedAnimal = animal;
+		}
+	});
+	return selectedAnimal;
+}
+*/
