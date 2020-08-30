a8e3c73beca7e01a670c1837b5fc35801030cb6a
TheVirtuoid <thevirtuoid@gmail.com>
2020-08-30 12:10:36 -0400

    Updates for J012

--- J021-set/delete.js
+++ J021-set/delete.js
@@ -2,29 +2,14 @@ const animals = [
 	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
 ];
 
+/*
 let animalSet = new Set(animals);
 const removedAnimal = animalSet.delete("Cat");
 console.log(`Did I remove the cat? ${removedAnimal ? "Yes!" : "No."}`);
+console.log(animalSet);
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 let animalSet = new Set(animals);
 const removedAnimal = animalSet.delete("Salamander");
 console.log(`Did I remove the salamander? ${removedAnimal ? "Yes!" : "No."}`);
-*/
+console.log(animalSet);
--- J021-set/has.js
+++ J021-set/has.js
@@ -2,54 +2,41 @@ const animals = [
 	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
 ];
 
+/*
 let animalSet = new Set(animals);
 const haveCat = animalSet.has("Cat");
 const haveSalamander = animalSet.has("Salamander");
 console.log(`Do I have a cat? ${haveCat ? "Yes!" : "No."}`);
 console.log(`Do I have a salamander? ${haveSalamander ? "Yes!" : "No."}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 /*
+
 let animalSet = new Set(animals);
 animalSet.add({type: "Salamander", name: "Sammy"});
 const haveSalamander = animalSet.has({type: "Salamander", name: "Sammy"});
 console.log(`Do I have a salamander? ${haveSalamander ? "Yes!" : "No."}`);
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 let animalSet = new Set(animals);
 let salamander = {type: "Salamander", name: "Sammy"};
 animalSet.add(salamander);
 const haveSalamander = animalSet.has(salamander);
 console.log(`Do I have a salamander? ${haveSalamander ? "Yes!" : "No."}`);
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
