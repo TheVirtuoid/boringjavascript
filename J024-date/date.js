6aab2a714d816cebef218703ff7cffef97b5516c
TheVirtuoid <thevirtuoid@gmail.com>
2020-09-20 11:47:44 -0400

    New project - J024-date

---
+++ J024-date/date.js
@@ -0,0 +1,24 @@
+const currentDateTime = new Date();
+console.log(`Current Date/Time: ${currentDateTime}\n`);
+
+// All the "get" functions
+console.log(`getDate()\t\t${currentDateTime.getDate()}`);
+console.log(`getDay()\t\t${currentDateTime.getDay()}`);
+console.log(`getFullYear()\t\t${currentDateTime.getFullYear()}`);
+console.log(`getHours()\t\t${currentDateTime.getHours()}`);
+console.log(`getMilliseconds()\t${currentDateTime.getMilliseconds()}`);
+console.log(`getMinutes()\t\t${currentDateTime.getMinutes()}`);
+console.log(`getMonth()\t\t${currentDateTime.getMonth()}`);
+console.log(`getSeconds()\t\t${currentDateTime.getSeconds()}`);
+console.log(`getTime()\t\t${currentDateTime.getTime()}`);
+console.log(`getTimezoneOffset()\t${currentDateTime.getTimezoneOffset()}`);
+console.log(`getYear()\t\t${currentDateTime.getYear()}`);
+console.log('------------------------------');
+console.log(`getUTCDate()\t\t${currentDateTime.getUTCDate()}`);
+console.log(`getUTCDay()\t\t${currentDateTime.getUTCDay()}`);
+console.log(`getUTCFullYear()\t${currentDateTime.getUTCFullYear()}`);
+console.log(`getUTCHours()\t\t${currentDateTime.getUTCHours()}`);
+console.log(`getUTCMilliseconds()\t${currentDateTime.getUTCMilliseconds()}`);
+console.log(`getUTCMinutes()\t\t${currentDateTime.getUTCMinutes()}`);
+console.log(`getUTCMonth()\t\t${currentDateTime.getUTCMonth()}`);
+console.log(`getUTCSeconds()\t\t${currentDateTime.getUTCSeconds()}`);
