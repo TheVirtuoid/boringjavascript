5af8c5c402f35d85cbd2732d9c2f8d6081266c15
TheVirtuoid <thevirtuoid@gmail.com>
2020-09-22 19:06:43 -0400

    Minor changes to constructor

--- J024-date/constructor.js
+++ J024-date/constructor.js
@@ -7,7 +7,7 @@ console.log(`The UNIX Timestamp:\t\t${unixTimestamp}`);
 const stringTimestamp = new Date('September 22, 2020 11:00:00');
 console.log(`The String Timestamp:\t\t${stringTimestamp}`);
 
-const paramTimestamp = new Date(2020, 8, 22, 11, 0, 0, 123);
+const paramTimestamp = new Date(2020, 8, 22, 11, 22, 43, 123);
 console.log(`The Params Timestamp:\t\t${paramTimestamp}`);
 
 const dateOnlyTimestamp = new Date(2020, 8, 22);
