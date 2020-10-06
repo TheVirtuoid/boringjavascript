3e169096c9fda4219a2728fc4cff22f1668f9883
TheVirtuoid <thevirtuoid@gmail.com>
2020-10-06 19:55:56 -0400

    Updates to J025-setTimeout

--- J025-setTimeout/setTimeout2.js
+++ J025-setTimeout/setTimeout2.js
@@ -1,15 +1,15 @@
 const results = document.getElementById('results');
 let myVar1 = 1;
 let myVar2 = 2;
-let myVar3 = 3;
+let myVar3 = 500;
 
 sendText(results, "Before setTimeout");
 const myTimeout = setTimeout(setTimeoutFunction, 3000, myVar1, myVar2, myVar3);
 sendText(results, "After setTimeout");
 
 
-function setTimeoutFunction (myVar1, myVar2, myVar3) {
-	sendText(results, `*** setTimeoutFunction has executed (${myVar1}, ${myVar2}, ${myVar3}) ***`);
+function setTimeoutFunction (incomingVar1, incomingVar2, incomingVar3) {
+	sendText(results, `*** Variables passed: (${incomingVar1}, ${incomingVar2}, ${incomingVar3}) ***`);
 }
 
 function sendText(dom, text) {
