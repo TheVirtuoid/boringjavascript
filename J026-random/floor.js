acae19bdb642211cbf8f0dbfa7fbbfcb0dac610b
TheVirtuoid <thevirtuoid@gmail.com>
2020-10-15 19:14:14 -0400

    J026 - Change for floor

--- J026-random/floor.js
+++ J026-random/floor.js
@@ -1,6 +1,24 @@
 const number = Math.floor(Math.random() * 10);
 console.log(`\nA number between 0 and 9: ${number}\n`);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 let numbers = [];
 for (let x = 0; x < 20; x++) {
