3140f5683ffcb1cd5dc67403b48d5e0f16d66f58
TheVirtuoid <thevirtuoid@gmail.com>
2020-10-22 16:53:53 -0400

    UPdates for J027

--- J027-slice/array.js
+++ J027-slice/array.js
@@ -13,7 +13,7 @@ const all = animals.slice();
 console.log('\nIndex Legend:');
 console.log('   0     1      2      3      4          5            6        7       8');
 console.log(JSON.stringify(animals));
-console.log('  -8    -7     -6     -5     -4         -3           -2       -1       0\n');
+console.log('  -9    -8     -7     -6     -5         -4           -3       -2      -1\n');
 
 console.log(`(3):      ${JSON.stringify(thirdToEnd)}`);
 console.log(`(3, 6):   ${JSON.stringify(thirdToSixth)}`);
