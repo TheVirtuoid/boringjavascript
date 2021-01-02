6dd3f5d48524c5d92dbbfd7498c3605255a8a5ae
TheVirtuoid <thevirtuoid@gmail.com>
2021-01-02 09:02:35 -0500

    Added code

--- J033-typeof/instanceof.js
+++ J033-typeof/instanceof.js
@@ -1,1 +1,36 @@
-// instanceof
+class BoringClass {
+	constructor () {
+		this.amIBored = true;
+	}
+
+	getBored () {
+		return this.amIBored;
+	}
+
+	setBoard (boredom) {
+		this.amIBored = boredom;
+	}
+}
+
+const myArray = [ 1, 2, 3 ];
+const myObject = { a: 1 };
+const boredom = new BoringClass();
+
+console.log(`myArray = Array?\t${myArray instanceof Array}`);
+console.log(`myObject = Object?\t${myObject instanceof Object}`);
+console.log(`boredom = BoringClass?\t${boredom instanceof BoringClass}`);
+
+/*
+class SuperBoring extends BoringClass {
+	constructor () {
+		super();
+		this.reallyBored = true;
+	}
+}
+
+const superBored = new SuperBoring();
+console.log(`superBored = SuperBoring?\t${superBored instanceof SuperBoring}`);
+console.log(`superBored = BoringClass?\t${superBored instanceof BoringClass}`);
+console.log(`superBored = Object?\t\t${superBored instanceof Object}`);
+*/
+// console.log(superBored.constructor.name);
