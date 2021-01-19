72d9cd8c4e960e3edefc2e02eab25807630b7fdc
TheVirtuoid <thevirtuoid@gmail.com>
2021-01-18 21:29:33 -0500

    Work on J034

--- J034-getelementbyid/getElementsByClassName.js
+++ J034-getelementbyid/getElementsByClassName.js
@@ -1,1 +1,20 @@
-// getElementsByClassName
+import { clearErrors, printErrors, validate, setupForm } from "./validate.js";
+
+setupForm();
+document.getElementById('profileForm').addEventListener('submit', processForm);
+
+function processForm(event) {
+	const errors = validate();
+	if (errors.length > 0) {
+		event.preventDefault();
+		printErrors(errors);
+		const allErrors = document.getElementsByClassName('error');
+		document.getElementById('numErrors').textContent = `${allErrors.length}`;
+		document.getElementById('numErrorsPlural').textContent = `${allErrors.length === 1 ? '' : 's'}`;
+		setupForm(false);
+	} else {
+		// normally, you would do nothing here. But I don't have server-side routines to
+		// handle a submit, so I'm killing the submit here.
+		event.preventDefault();
+	}
+}
