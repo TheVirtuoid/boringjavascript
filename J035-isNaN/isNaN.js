9224ca1376bc2d7b24101d61a03f9794794106bf
TheVirtuoid <thevirtuoid@gmail.com>
2021-02-22 07:56:15 -0500

    Populated J035-isNaN

--- J035-isNaN/isNaN.js
+++ J035-isNaN/isNaN.js
@@ -1,1 +1,5 @@
-// isNaN
+console.log(isNaN(1));					// false
+console.log(isNaN("virtuoid"));	// true
+console.log(isNaN("1"));				// false
+console.log(isNaN(0x0a));				// false
+console.log(isNaN("0x0a"));			// false
---
+++ J035-isNaN/other.js
@@ -0,0 +1,10 @@
+console.log(isNaN(true));		// false
+console.log(isNaN(null));		// false
+console.log(isNaN(NaN));
+
+
+/*
+console.log(1 + true);		// prints 2
+console.log(1 + false);		// prints 1
+console.log(1 + null);		// prints 1
+*/
