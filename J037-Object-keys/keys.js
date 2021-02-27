e739e89969b1d6d0a6c9858bc328639e71e55a13
TheVirtuoid <thevirtuoid@gmail.com>
2021-02-27 11:33:39 -0500

    J037 initial coomit

---
+++ J037-Object-keys/keys.js
@@ -0,0 +1,14 @@
+const myCat = {
+	name: "Fluffy",
+	breed: "Himalayan",
+	arms: 0,
+	legs: 4,
+	tail: true,
+	weight: 3.58
+};
+
+const myCatKeys = Object.keys(myCat);
+
+console.log('The keys of the myCat object are:');
+console.log(myCatKeys);
+
