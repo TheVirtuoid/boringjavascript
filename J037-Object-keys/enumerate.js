daf59fcf8abd7ab0112b614caf55cf3f25e3eeac
TheVirtuoid <thevirtuoid@gmail.com>
2021-03-14 11:14:43 -0400

    Added enumerate

---
+++ J037-Object-keys/enumerate.js
@@ -0,0 +1,31 @@
+class Animal {
+	constructor () {
+		this.body = true;
+		this.head = true;
+		this.legs = 0;
+		this.alive = false;
+	}
+
+	born () {
+		this.alive = true;
+	}
+}
+
+const myCat = new Animal();
+console.log(Object.keys(myCat));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(myCat.constructor);
