3c8e2b345b46e6668fd905e658c3983bba17884d
TheVirtuoid <thevirtuoid@gmail.com>
2021-04-05 09:53:04 -0400

    Update to Truthy

--- J040-if/truthy.js
+++ J040-if/truthy.js
@@ -2,19 +2,10 @@ const animals = [
 	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
 ];
 
-const myAnimal = animals[Math.floor(Math.random() * animals.length)];
-/*
-const isMyCat = myAnimal === "Cat"
-if (isMyCat) {
+const myAnimalIndex = Math.floor(Math.random() * animals.length);
+
+if (myAnimalIndex) {
 	console.log('\nMy animal is a Cat!');
 } else {
-	console.log(`\nMy animal is NOT a Cat! It's a ${myAnimal}.`);
-}
-*/
-
-
-if (myAnimal) {
-	console.log('\nMy animal is a Cat!');
-} else {
-	console.log(`\nMy animal is NOT a Cat! It's a ${myAnimal}.`);
+	console.log(`\nMy animal is NOT a Cat! It's a ${animals[myAnimalIndex]}.`);
 }
