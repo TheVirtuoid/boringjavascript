c07164552adfa3afe34618c9c372c79435006010
TheVirtuoid <thevirtuoid@gmail.com>
2021-04-05 09:14:41 -0400

    Initial Commit - J042-matchAll

---
+++ J042-string-matchAll/matchAll.js
@@ -0,0 +1,7 @@
+const animals = "Cat,Dog,Horse,Cow,Coyote,Road Runner,Dolphin,Whale,Lizard,Wildcat,Dog,Horse,Cow,Bobcat,Whale";
+
+const myCats = animals.matchAll(/,?([A-Za-z]*?[Cc]at),?/g);
+
+for (const cat of myCats) {
+	console.log(cat);
+}
