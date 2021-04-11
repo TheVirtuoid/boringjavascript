ad9d75a943734e0395e5fbd3fa9842f9e6a330c5
TheVirtuoid <thevirtuoid@gmail.com>
2021-04-11 11:14:15 -0400

    Mods to stringify.js

--- J041-JSON/stringify.js
+++ J041-JSON/stringify.js
@@ -12,8 +12,30 @@ const myCatInJSON = JSON.stringify(myCat);
 console.log(myCatInJSON);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const animals = [
 	"Cat", "Dog", "Horse", "Cow", "Coyote", "Road Runner", "Dolphin", "Whale", "Lizard"
 ];
 
 console.log(JSON.stringify(animals));
+*/
