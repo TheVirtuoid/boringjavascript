b4e576877db5c3f4802afae77eca4d65d0ab4177
TheVirtuoid <thevirtuoid@gmail.com>
2021-05-02 11:50:11 -0400

    Initial Commit - J047-bind

---
+++ J047-bind/bind.js
@@ -0,0 +1,21 @@
+const animals = [
+	{ type: "Cat", name: "Fluffy" },
+	{ type: "Dog", name: "Fido" },
+	{ type: "Horse", name: "Mr. Ed" },
+	{ type: "Cow", name: "Betsy" },
+	{ type: "Coyote", name: "Wile E." },
+	{ type: "Road Runner", name: "Beep Beep" },
+	{ type: "Dolphin", name: "Flipper" },
+	{ type: "Whale", name: "Moby Dick" },
+	{ type: "Lizard", name: "Larry" }
+];
+
+function getAnimalName (animalType) {
+	const animal =  this.find( animal => animal.type === animalType);
+	return animal.name;
+}
+
+const myAnimal = "Cat";
+const myAnimalName = getAnimalName(myAnimal);
+
+console.log(`\nThe name of my ${myAnimal} is ${myAnimalName}.`);
