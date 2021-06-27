b093a52351f19fc4915cfcbd784d5e32268ee234
TheVirtuoid <thevirtuoid@gmail.com>
2021-06-27 13:46:04 -0400

    initial commit

---
+++ J055-weakset/get.js
@@ -0,0 +1,17 @@
+const myHouse = new WeakSet();
+
+const cat = { name: 'Fluffy', age: 2 };
+const dog = { name: 'Rover', age: 5 };
+const lizard = { name: 'Larry', age: 3 };
+
+myHouse.add(cat);
+myHouse.add(dog);
+
+console.log(`Is there a cat in my house?\t${myHouse.has(cat) ? 'Yes' : 'No'}`);
+console.log(`Is there a dog in my house?\t${myHouse.has(dog) ? 'Yes' : 'No'}`);
+console.log(`Is there a lizard in my house?\t${myHouse.has(lizard) ? 'Yes' : 'No'}`);
+
+
+myHouse.delete(dog);
+console.log('\n-------- The Dog Was Adopted ---------');
+console.log(`Is there a dog in my house?\t${myHouse.has(dog) ? 'Yes' : 'No'}`);
