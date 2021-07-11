b7daa66843db77217e9a83bc19a0f78bec6dea1b
TheVirtuoid <thevirtuoid@gmail.com>
2021-07-11 09:05:26 -0400

    INitial Commit - J060-charat

---
+++ J060-charat/charat.js
@@ -0,0 +1,10 @@
+const myCat = "Fluffy";
+const thirdCharacter = myCat.charAt(2);
+console.log(`The third character in my cat's name is "${thirdCharacter}".`);
+
+console.log(`-1 = "${myCat.charAt(-1)}".`);
+console.log(`1000 = "${myCat.charAt(1000)}".`);
+console.log(`String 2 = "${myCat.charAt("2")}".`);
+console.log(`true = "${myCat.charAt(true)}".`);
+console.log(`false = "${myCat.charAt(false)}".`);
+
