9a8931480ac8380018765d636eef5008a6c7096c
TheVirtuoid <thevirtuoid@gmail.com>
2021-07-26 13:55:45 -0400

    Completed work - J058-import

--- J058-import/ImportAnimal.js
+++ J058-import/ImportAnimal.js
@@ -1,13 +1,15 @@
 export default class Animal {
 	name = "";
 	type = "";
+	age = 0;
 	constructor(args) {
 		const { name, type } = args;
 		this.name = name;
 		this.type = type;
+		this.age = Math.floor(Math.random() * 10) + 2;
 	}
 
 	toSuperString () {
-		return `The animal's name is "${this.name}", and its type is a "${this.type}."`;
+		return `The animal's name is "${this.name}", its type is a "${this.type}, and its age is ${this.age} years old."`;
 	}
 }
---
+++ J058-import/import-html.js
@@ -0,0 +1,7 @@
+import Animal from './ImportAnimal.js';
+
+const myCat = new Animal({ name: "Fluffy", type: "cat" });
+
+document.getElementById('type').textContent = myCat.type;
+document.getElementById('name').textContent = myCat.name;
+document.getElementById('age').textContent = myCat.age.toString();
