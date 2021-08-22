47b8100640320f118ef2bb3dc0044da6faa7ac62
TheVirtuoid <thevirtuoid@gmail.com>
2021-08-22 13:31:37 -0400

    Update - J060-charat

--- J060-charat/charat.js
+++ J060-charat/charat.js
@@ -1,10 +1,37 @@
 const myCat = "Fluffy";
 const thirdCharacter = myCat.charAt(2);
-console.log(`The third character in my cat's name is "${thirdCharacter}".`);
+console.log(`\nThe third character in my cat's name is "${thirdCharacter}".`);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log(`No arg = "${myCat.charAt()}".`);
 console.log(`-1 = "${myCat.charAt(-1)}".`);
 console.log(`1000 = "${myCat.charAt(1000)}".`);
 console.log(`String 2 = "${myCat.charAt("2")}".`);
 console.log(`true = "${myCat.charAt(true)}".`);
 console.log(`false = "${myCat.charAt(false)}".`);
-
+console.log(`Object = "${myCat.charAt({ cat: 1 })}".`);
+*/
