da4b3b4589dfb359658c3faeb1e74a87aca1ae52
TheVirtuoid <thevirtuoid@gmail.com>
2021-09-10 17:02:17 -0400

    INitial commit - J063-array-from

---
+++ J063-array-from/from.js
@@ -0,0 +1,27 @@
+console.log('------------------- string ----------------')
+
+const myCharacters = Array.from('Fluffy');
+console.log(myCharacters);
+
+console.log('------------------- array -----------------')
+const myArray = Array.from([ 1, 2, 3, 4]);
+console.log(myArray);
+
+console.log('------------------- map -------------------')
+const myMap = new Map();
+myMap.set('cat', 'Fluffy');
+myMap.set('dog', 'Rover');
+myMap.set('lizard', 'Larry');
+const myArrayMap = Array.from(myMap);
+console.log(myArrayMap);
+console.log(Array.from(myMap.keys()));
+console.log(Array.from(myMap.values()));
+
+console.log('------------------- set -------------------')
+const mySet = new Set();
+mySet.set('Fluffy');
+mySet.set('Rover');
+mySet.set('Larry');
+const myArraySet = Array.from(mySet);
+console.log(myArraySet);
+
