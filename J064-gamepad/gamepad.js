dfec6f8565a56071fadd9b7878a2e6772bd478be
TheVirtuoid <thevirtuoid@gmail.com>
2021-09-12 08:51:00 -0400

    Updates to J064-gamepad

--- J064-gamepad/gamepad.js
+++ J064-gamepad/gamepad.js
@@ -2,26 +2,33 @@ let gamepadIndex = -1;
 const domConnected = document.getElementById('connected');
 const domId = document.getElementById('id');
 const domIds = new Map();
+const mapping = new Map();
 
-const buttonList = [
+mapping.set('standard', [
 		'A', 'B', 'X', 'Y', 'Left-Top', 'Right-Top', 'Left-Trigger', 'Right-Trigger',
 		'Home-Select', 'Start', 'Left-Stick', 'Right-Stick', 'DPad-Up', 'DPad-Down',
 		'DPad-Left', 'DPad-Right', 'Unknown'
-]
+]);
 
-buttonList.forEach( (buttonName) => {
-	domIds.set(buttonName, {
-		dom: document.getElementById(buttonName),
-		display: buttonName.includes('Trigger') ? 'number' : 'boolean'
+const buildDomIds = (controllerMapping) => {
+	const buttonMapping = mapping.get(controllerMapping);
+	buttonMapping.forEach( (buttonName) => {
+		domIds.set(buttonName, {
+			dom: document.getElementById(buttonName),
+			display: buttonName.includes('Trigger') ? 'number' : 'boolean'
+		});
 	});
-});
+
+};
 
 const gamepadConnected = (event) => {
 	gamepadIndex = event.gamepad.index;
 	console.log(navigator.getGamepads()[gamepadIndex]);
 	domConnected.textContent = "Yes";
+	buildDomIds(navigator.getGamepads()[gamepadIndex].mapping);
 	requestAnimationFrame(updateTable);
 };
+
 const gamepadDisconnected = (event) => {
 	domConnected.textConent = "No";
 };
@@ -30,12 +37,15 @@ const updateTable = () => {
 	const gamepad = navigator.getGamepads()[gamepadIndex];
 	domConnected.textContent = gamepad.connected ? "Yes" : "No";
 	domId.textContent = gamepad.id;
+	const buttonMapping = mapping.get(gamepad.mapping);
 	gamepad.buttons.forEach( (button, index) => {
-		const { dom, display } = domIds.get(buttonList[index]);
+		const { dom, display } = domIds.get(buttonMapping[index]);
 		if (display === "boolean") {
 			dom.classList.remove('on', 'off');
 			dom.classList.add(button.pressed ? 'on' : 'off');
 			dom.textContent = button.pressed ? 'on' : 'off';
+		} else if (display === "number") {
+			dom.textContent = button.value.toFixed(2);
 		}
 		// domIds.get(buttonList[index]).textContent = `${button.pressed},${button.touched},${button.value}`;
 	});
