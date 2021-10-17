45c9bf1323e2ebc8f3789cdabe5e87e3bfdcadcb
TheVirtuoid <thevirtuoid@gmail.com>
2021-10-17 09:51:56 -0400

    Fixes to loading dock: J071-channel-message

--- J071-channel-message/loading-dock/loading-dock.js
+++ J071-channel-message/loading-dock/loading-dock.js
@@ -1,4 +1,12 @@
 let iFramePort = null;
+const animalToAdd = document.getElementById('animal-to-add');
+const addAnimal = document.getElementById('add-animal');
+const animalToTransfer = document.getElementById('animal-to-transfer');
+const transferAnimal = document.getElementById('transfer-animal');
+const zooListing = document.getElementById('zoo-listing');
+
+let transferAnimalTableElement = null;
+
 const setDisplayForIFrame = (event) => {
 	switch (event.data) {
 		case 'init':
@@ -15,4 +23,31 @@ const receivedMessageFromParent = (event) => {
 	console.log(event);
 }
 
+const addTheAnimal = (event) => {
+	const [type, name] = animalToAdd.value.split(',');
+	zooListing.insertAdjacentHTML('beforeend', `<tr><td>${type}</td><td>${name}</td></tr>`);
+}
+
+const transferTheAnimal = (event) => {
+	//
+	// here it is written to a database somewhere.
+	//
+	if (transferAnimalTableElement) {
+		transferAnimalTableElement.remove();
+		transferAnimalTableElement = null;
+		animalToTransfer.value = "";
+	}
+
+	console.log(event);
+}
+
+const addAnimalToTransferInput = (event) => {
+	transferAnimalTableElement = event.target.parentElement;
+	const transferAnimalText = `${transferAnimalTableElement.firstChild.textContent},${transferAnimalTableElement.firstChild.nextSibling.textContent}`;
+	animalToTransfer.value = transferAnimalText;
+}
+
 window.addEventListener('message', setDisplayForIFrame);
+addAnimal.addEventListener('click', addTheAnimal);
+transferAnimal.addEventListener('click', transferTheAnimal);
+zooListing.addEventListener('click', addAnimalToTransferInput);
