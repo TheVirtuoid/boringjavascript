0fd566b696a003598a35bc5009b6acf1e4db0053
TheVirtuoid <thevirtuoid@gmail.com>
2021-10-24 09:01:09 -0400

    refactor J069-generator

--- J069-generator/event/worker-event.js
+++
@@ -1,9 +0,0 @@
-const sendData = (data) => {
-	data.forEach( (record, index) => {
-		postMessage({ index, record });
-	});
-}
-
-fetch('./animals.json')
-.then(response => response.json())
-.then(data => sendData(data));
---
+++ J069-generator/fixed/reader.js
@@ -0,0 +1,21 @@
+export default class Reader {
+	isReady = false;
+	data = null;
+
+	getData() {
+		return new Promise( (resolve, reject) => {
+			fetch('./animals-other.json')
+					.then(response => response.json())
+					.then(data => this.saveData(data))
+					.then(resolve)
+					.catch(reject)
+		});
+	}
+
+	saveData(data) {
+		this.isReady = true;
+		this.data = data;
+		return this.data;
+	}
+
+}
