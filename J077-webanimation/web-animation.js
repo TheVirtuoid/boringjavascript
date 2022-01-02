f2ef036c3b03a82529b706d46802055f7d22b398
TheVirtuoid <thevirtuoid@gmail.com>
2022-01-02 12:52:10 -0500

    Initial commit - J077-webanimation

---
+++ J077-webanimation/web-animation.js
@@ -0,0 +1,44 @@
+const go = document.getElementById('go');
+const reset = document.getElementById('reset');
+const cat = document.getElementById('cat');
+const range = document.getElementById('range');
+const rangeValue = document.getElementById('range-value');
+
+let catAnimation;
+
+const keyFrames = [
+	{ marginLeft: 0, offset: 0 },
+	{ marginLeft: '40rem', offset: 1 }
+]
+
+const timing = {
+	duration: 3000,
+	easing: 'linear',
+	fill: 'forwards'
+}
+
+const moveTheCat = () => {
+	// cat.classList.add('move-me');
+	catAnimation = cat.animate(keyFrames, timing);
+};
+
+const changeTheValue = () => {
+	rangeValue.textContent = range.value;
+	timing.duration = parseFloat(range.value) * 1000;
+}
+
+const resetCat = () => {
+	// cat.classList.remove('move-me');
+	if (catAnimation) {
+		catAnimation.cancel();
+	}
+}
+
+go.addEventListener('click', moveTheCat);
+reset.addEventListener('click', resetCat);
+range.addEventListener('input', changeTheValue);
+
+
+
+
+// timing options: keyframeweffect
