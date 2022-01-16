7442ca3d2dd5813d3dde3e1ba5a74416b11f9864
TheVirtuoid <thevirtuoid@gmail.com>
2022-01-16 14:30:35 -0500

    Updates to J077-webanimation

--- J077-webanimation/web-animation.js
+++ J077-webanimation/web-animation.js
@@ -1,6 +1,7 @@
 const go = document.getElementById('go');
 const reset = document.getElementById('reset');
 const cat = document.getElementById('cat');
+
 const range = document.getElementById('range');
 const rangeValue = document.getElementById('range-value');
 
@@ -9,13 +10,69 @@ let catAnimation;
 const keyFrames = [
 	{ marginLeft: 0, offset: 0 },
 	{ marginLeft: '40rem', offset: 1 }
-]
+];
 
 const timing = {
 	duration: 3000,
 	easing: 'linear',
 	fill: 'forwards'
+};
+
+
+const moveTheCat = () => {
+	catAnimation = cat.animate(keyFrames, timing);
+};
+
+const resetCat = () => {
+	if (catAnimation) {
+		catAnimation.cancel();
+	}
 }
+
+const changeTheValue = () => {
+	rangeValue.textContent = range.value;
+	timing.duration = parseFloat(range.value) * 1000;
+}
+
+go.addEventListener('click', moveTheCat);
+reset.addEventListener('click', resetCat);
+range.addEventListener('input', changeTheValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const range = document.getElementById('range');
+const rangeValue = document.getElementById('range-value');
+
+let catAnimation;
+
+const keyFrames = [
+	{ marginLeft: 0, offset: 0 },
+	{ marginLeft: '40rem', offset: 1 }
+];
+
+const timing = {
+	duration: 3000,
+	easing: 'linear',
+	fill: 'forwards'
+};
 
 const moveTheCat = () => {
 	// cat.classList.add('move-me');
@@ -34,11 +91,6 @@ const resetCat = () => {
 	}
 }
 
-go.addEventListener('click', moveTheCat);
-reset.addEventListener('click', resetCat);
 range.addEventListener('input', changeTheValue);
 
-
-
-
-// timing options: keyframeweffect
+ */
