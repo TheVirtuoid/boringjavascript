1ac6a270eff16bf044670383eb3fbfa2acc78abe
TheVirtuoid <thevirtuoid@gmail.com>
2022-04-24 14:37:59 -0400

    Initial commit - J091-for-in-of

---
+++ J091-for-in-of/for-in.js
@@ -0,0 +1,71 @@
+const myCat = { "type": "Cat", "name": "Fluffy", "class": "Mammalia"};
+
+const newProperty = Symbol("tail");
+myCat[newProperty] = true;
+
+console.log(myCat);
+
+for(const catProperty in myCat) {
+	console.log(`The property "${catProperty}" contains the value "${myCat[catProperty]}".`);
+}
+
+const animals = [
+	{ "type": "Cat", "name": "Fluffy", "class": "Mammalia"},
+	{ "type": "Dog", "name": "Fido", "class": "Mammalia"},
+	{ "type": "Horse", "name": "Mr. Ed", "class": "Mammalia"},
+	{ "type": "Cow", "name": "Betsy", "class": "Mammalia"},
+	{ "type": "Coyote", "name": "Wile E.", "class": "Mammalia"},
+	{ "type": "Road Runner", "name": "Beep Beep", "class": "Aves"},
+	{ "type": "Dolphin", "name": "Flipper",  "class": "Mammalia"},
+	{ "type": "Whale", "name": "Moby Dick", "class": "Mammalia"},
+	{ "type": "Lizard", "name": "Larry", "class": "Reptilia"}
+];
+
+animals.zooName = 'MyZoo';
+
+for(const animal in animals) {
+	console.log(animal);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const newProperty = Symbol("tail");
+myCat[newProperty] = true;
+
+console.log(myCat);
+
+ */
+
+/*
+const animals = [
+	{ "type": "Cat", "name": "Fluffy", "class": "Mammalia"},
+	{ "type": "Dog", "name": "Fido", "class": "Mammalia"},
+	{ "type": "Horse", "name": "Mr. Ed", "class": "Mammalia"},
+	{ "type": "Cow", "name": "Betsy", "class": "Mammalia"},
+	{ "type": "Coyote", "name": "Wile E.", "class": "Mammalia"},
+	{ "type": "Road Runner", "name": "Beep Beep", "class": "Aves"},
+	{ "type": "Dolphin", "name": "Flipper",  "class": "Mammalia"},
+	{ "type": "Whale", "name": "Moby Dick", "class": "Mammalia"},
+	{ "type": "Lizard", "name": "Larry", "class": "Reptilia"}
+];
+
+for(const animal in animals) {
+	console.log(animal);
+}
+*/
