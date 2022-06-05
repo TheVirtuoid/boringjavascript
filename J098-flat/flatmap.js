b0c116c285086ea93f4ac4c32ca504d979ead311
TheVirtuoid <thevirtuoid@gmail.com>
2022-06-05 09:21:28 -0400

    Initial commit - J098-flat

---
+++ J098-flat/flatmap.js
@@ -0,0 +1,46 @@
+const myAnimals = [
+	[{ name: 'Fluffy', type: 'Cat' }],
+	[
+		[
+			{ name: 'Squeeky', type: 'Chihuahua' },
+			{ name: 'Godzilla', type: 'Chihuahua'}
+		],
+		[
+			{ name: 'Mothra', type: 'Great Dane' },
+			{ name: 'Itty Bit', type: 'Great Dane' }
+		]
+	],
+	[{ name: 'Mr. Ed', type: 'Horse' }]
+];
+console.log(`Number of animals in my zoo: ${myAnimals.length}`);
+console.log(myAnimals);
+
+let critterCount = 0;
+const allAnimals = myAnimals.flatMap((animals) => {
+	animals.forEach((animal) => animal.critterCount = ++critterCount);
+	return animals;
+});
+console.log(`Number of animals in my flattened zoo: ${allAnimals.length}`);
+console.log(allAnimals);
+
+
+/*
+const myAnimals = [
+	[{ name: 'Fluffy', type: 'Cat' }],
+	[
+		{ name: 'Fido', type: 'Dog' },
+		{ name: 'Rolf', type: 'Dog' }
+	],
+	[{ name: 'Mr. Ed', type: 'Horse' }]
+];
+console.log(`Number of animals in my zoo: ${myAnimals.length}`);
+console.log(myAnimals);
+
+let critterCount = 0;
+const allAnimals = myAnimals.flatMap((animals) => {
+	animals.forEach((animal) => animal.critterCount = ++critterCount);
+	return animals;
+});
+console.log(`Number of animals in my flattened zoo: ${allAnimals.length}`);
+console.log(allAnimals);
+*/
