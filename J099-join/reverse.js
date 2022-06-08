de4428a44fc865c2f87d844c82cb32185a208641
TheVirtuoid <thevirtuoid@gmail.com>
2022-06-08 18:10:15 -0400

    Updates to J099-join

--- J099-join/reverse.js
+++ J099-join/reverse.js
@@ -1,6 +1,43 @@
-const statement = "I shot an arrow into the air it fell to earth into a bear";
+const statement = "I shot an arrow into the air it fell to earth I know not where";
 
-const words = statement.split(" ");
+/*const words = statement.split(' ');
+const backwardsWords = words.map((word) => {
+	return word.split('').reverse().join('');
+});
+backwardsWords.reverse();
+
+const backwardsStatement = backwardsWords.join(' ');
+
+console.log(backwardsStatement);*/
+
+console.log(statement.split('').reverse().join(''));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const words = statement.split(' ');
 const backwardsWords = words.map((word) => {
 	return word.split('').reverse().join('');
 });
@@ -9,32 +46,7 @@ backwardsWords.reverse();
 const backwardsStatement = backwardsWords.join(' ');
 
 console.log(backwardsStatement);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
 
 
 /*
