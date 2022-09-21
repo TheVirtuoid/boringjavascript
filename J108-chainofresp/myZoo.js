1112ab6b07b6e8e6bd575ad401625de815e17a8e
TheVirtuoid <thevirtuoid@gmail.com>
2022-09-20 20:52:27 -0400

    Initial Commit - J108-chainofresp

--- J108-chainofresp/myZoo.js
+++ J108-chainofresp/myZoo.js
@@ -1,9 +1,43 @@
 import ZooCentral from "./ZooCentral.js";
 
 const myAnimals = [];
-
 const zooCentral = new ZooCentral();
+let animalCount = 0;
+const mainZoo = zooCentral.firstZoo;
 
+while (animalCount < 10) {
+	const transferAnimal = mainZoo.transferAnimal();
+	const { type = '', name = 'No Animal', zooName = '' } = transferAnimal;
+	myAnimals.push({ type, name, zooName });
+	animalCount++;
+}
+
+console.log(`\n${'Day'.padEnd(5)}${'Name'.padEnd(20)}${'Type'.padEnd(20)}Zoo`);
+console.log(`${'-'.repeat(4)} ${'-'.repeat(19)} ${'-'.repeat(19)} ${'-'.repeat(19)}`);
+myAnimals.forEach((animal, day) => {
+	const { type, name, zooName} = animal;
+	console.log(`${day.toString().padEnd(5)}${name.padEnd(20)}${type.padEnd(20)}${zooName}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const myAnimals = [];
+const zooCentral = new ZooCentral();
 let animalCount = 0;
 
 while (animalCount < 10) {
@@ -19,3 +53,4 @@ myAnimals.forEach((animal, day) => {
 	const { type, name, zooName} = animal;
 	console.log(`${day.toString().padEnd(5)}${name.padEnd(20)}${type.padEnd(20)}${zooName}`);
 });
+*/
